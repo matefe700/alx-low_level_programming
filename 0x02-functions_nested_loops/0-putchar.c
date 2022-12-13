de9733d1f3457162fd1f517a6a7f15dfcd4ed88a
Q P <qp@Qs-MacBook-Air.local>
2022-12-13 15:28:37 +0300

    0x02

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char string[] = "_putchar";
+	int i = 0;
 
-int main(void) {
-    printf("_putchar\n");
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
