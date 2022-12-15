ec3ed2fffe7d6043bc30cf02f18501bf86be31da
Q P <qp@Qs-MacBook-Air.local>
2022-12-15 16:26:16 +0300

    4

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,43 +6,25 @@
  * Description: Can only use _putchar to print.
  */
 
-void print_number(int n)
+void print_number(n)
 {
-	int m; /* power of 10 */
-	int c; /* boolean check */
-	int num; /* convert int to long */
+	int m = 1;
+	int num = n;
 
-	num = n;
-	/* negatives */
 	if (num < 0)
 	{
 		num *= -1;
 		_putchar('-');
 	}
 
-	/* count up */
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
+	int t = num;
 
-	/* count down */
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+	for (; t > 0; t /= 10,  m *= 10)
+		;
+
+	m /= 10;
+
+	for (; m > 0; m /= 10)
+		_putchar ('0' + (num / m) % 10);
+
 }
