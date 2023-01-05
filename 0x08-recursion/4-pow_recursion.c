2b823541713ae3ae81a17c52bf113227557a22c8
Q P <qp@Qs-MacBook-Air.local>
2023-01-05 03:10:14 +0300

    0x08

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _pow_recursion - return the value of `x` raised to power of `y`
--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * is_palindrome - check if a string is a palindrome
