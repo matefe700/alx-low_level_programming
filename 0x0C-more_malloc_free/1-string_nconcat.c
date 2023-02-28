980b0087c9369fee919917d558b02a5e10103987
Q P <qp@Qs-MacBook-Air.local>
2023-02-28 16:53:48 +0300

    0x0c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	for (i = 0, c = 0; i < (strlen1 + (int) n); i++)
 	{
 		if (i < strlen1)
 			p[i] = s1[i];
@@ -35,4 +35,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (p);
 }
+
 
