6201481967c0d15454a586472fea836a57dd9b13
Q P <qp@Qs-MacBook-Air.local>
2023-07-02 19:18:59 +0100

    alx

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,41 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
+
 /**
- * add_node_end - add a new node at the end of `list_t` list
- * @head: double pointer to head
- * @str: string to duplicate into new node
- * Return: Address of the new element or NULL if failed
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Pointer to the head of the list
+ * @str: String to be duplicated and added as the new node's str member
+ *
+ * Return: The address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current;
-	list_t *new_node;
-	int c;
-
-	current = *head;
-	while (current && current->next != NULL)
-		current = current->next;
-
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
+	list_t *new_node, *current;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = c;
+
 	new_node->next = NULL;
 
-	if (current)
-		current->next = new_node;
-	else
+	if (*head == NULL)
+	{
 		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+
+		current->next = new_node;
+	}
+
 	return (new_node);
 }
